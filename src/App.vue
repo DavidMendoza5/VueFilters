7c2814a29107f89e85184d2b3388b675177004ea
David <cruz-azul99@hotmail.com>
2021-01-25 11:32:54 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="badge bg-secondary">
+      <!-- La barra vertical indica que se va a aplicar un filtro a todo lo que esté antes de ella, en este caso a mensaje -->
+      <span>{{ mensaje | mayuscula }}</span>
+      <hr>
+      <!-- Uso de un filtro global -->
+      <span>{{ mensaje | suspensivos }}</span>
+      <hr>
+      <!-- Uso de más de un filtro, recordar que los filtros usan todo lo que tengan a la izquierda -->
+      <span>{{ mensaje | suspensivos | mayuscula }}</span>
+      <hr>
+      <span>Mensaje sin filtro: {{ mensaje }}</span>
+      <hr>
+      <span>Temperatura: {{ temperatura | aFahrenheit }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      mensaje: 'Hola mundo desde Vue',
+      temperatura: 20
+    }
+  },
+  // Definimos nuestros filtros
+  filters: {
+    mayuscula(mensajeParaConvertir) {
+      return mensajeParaConvertir.toUpperCase()
+    },
+    aFahrenheit(temperatura) {
+      return ((9/5)*temperatura)+32+'F'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Definimos un filtro global
+Vue.filter('suspensivos', (texto) => {
+  return texto.substring(0,10) + '...'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
